8db2897e3e2b754e535adcc579f8945b344fbacd
cesare.soldini <cesare.soldini@accurat.it>
2024-11-07 12:36:49 +0100

    Shorten backend logs

--- server.js
+++ server.js
@@ -14,12 +14,10 @@ app.use(express.json());
 //app.use(express.static("public"));
 
 app.get('/seqviz.js', function (req, res) {
-  console.log('seqviz.js');
   res.sendFile(path.join(__dirname, 'public', 'seqviz.js'));
 });
 
 app.get('/Status', function (req, res) {
-  console.log('Status');
   res.status(200).send('The plugin is up and running');
 });
 
@@ -37,7 +35,6 @@ app.get('/test/sample-data/:file', (req, res) => {
 
 app.post('/Evaluate', function (req, res) {
   let type = req.body.type.toString();
-  console.log('evaluate ' + type);
   if (type === 'Component' || type === 'ComponentDefinition') {
     res.status(200).send('The app can handle this input');
   } else {
@@ -69,15 +66,16 @@ app.all('/Run', async (req, res) => {
     req.body?.top_level?.toString() ?? 'https://synbiohub.org/public/igem/BBa_B0012/1';
 
   let hostAddr = req.get('host');
-  console.log('run url=' + url + ' top=' + top_level + ' hostAddr=' + hostAddr);
+  console.log('Run: url=' + url + ' top=' + top_level + ' hostAddr=' + hostAddr);
   try {
     console.log(`Get SBOL file content string...`);
-    const csv = await getFileData(url);
-    console.log(`SBOL file content string:\n${csv}`);
-    console.log(`Parse SBOL file to get data for sequence view rendering`);
-    const { displayList, parts } = await filesToParts(csv, {
+    const xml = await getFileData(url);
+    console.log(`SBOL file content (${xml.length}) begins with: ${xml.slice(0, 100)}...`);
+    console.log(`Parse SBOL file...`);
+    const { displayList, parts } = await filesToParts(xml, {
       topLevel: top_level,
     });
+    console.log('Parsed:', { displayList, parts: `Array(${parts.length})` });
 
     // Remove color, to clear out that we're setting it on the frontend
     parts.forEach((part) =>
@@ -138,4 +136,4 @@ function getFileData(url) {
   });
 }
 
-app.listen(port, () => console.log(`Example app listening at http://${addr}:${port}`));
+app.listen(port, () => console.log(`App listening at http://${addr}:${port}`));
